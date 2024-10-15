df8bb102afa3749cb9e2a5716c0154dd0e13307d
13rian Vargas <bsvargasc13@gmail.com>
2024-10-14 21:18:51 -0500

    DONE - Clase TPIntegrador
    - Agrego objetos de Ventana en el main

---
+++ TPIntegrador/src/logic/TPIntegrador.java
@@ -0,0 +1,12 @@
+package logic;
+
+import gui.Ventana;
+
+public class TPIntegrador {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true); //Hacer visible con ventana emergente
+        ventana.setLocationRelativeTo(null); //Centrar ventana emergente
+    }
+}
